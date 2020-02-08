96402404caccdcb02e54ed289690c9f5c32ab7ee
Hogan <hogan@dcpt.online>
2020-02-08 13:26:13 +0800

    添加immuatable和selector

---
+++ src/selectors/index.js
@@ -0,0 +1,22 @@
+export const getText = state => state.get("text");
+
+export const getFilter = state => state.get("filter");
+
+export const getVisibleTodos = state => {
+  const data = state.getIn(["todos", "data"]);
+  // const {
+  //   todos: { data },
+  //   filter
+  // } = state;
+  const filter = state.get("filter");
+  switch (filter) {
+    case "all":
+      return data;
+    case "completed":
+      return data.filter(t => t.get("completed"));
+    case "active":
+      return data.filter(t => !t.get("completed"));
+    default:
+      return new Error("Unknown filter:" + filter);
+  }
+};
